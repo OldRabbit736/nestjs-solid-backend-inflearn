539711daaf7c8b1c6629b88b147a8425c8e06f64
OldRabbit736 <sylvan0212@gmail.com>
2022-03-17 19:33:00 +0900

    Applied global validationpipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
-import { SuccessInterceptor } from './common/interceptors/success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.APPLICATION_PORT;
   await app.listen(port);
 }
