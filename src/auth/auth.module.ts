581c2cb74470d9feb93ff3d064c889b47ac33a9b
OldRabbit736 <sylvan0212@gmail.com>
2022-03-19 21:46:11 +0900

    created jwt strategy files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule.register({ session: false }), // session cookie 사용 여부?
+    // JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1y' } }),
+  ],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
