30d6b23c0a9b3e582f9d4656836aee5ddc3b12a4
OldRabbit736 <sylvan0212@gmail.com>
2022-03-20 11:22:29 +0900

    change tsconfig to have nullstrictcheck be true

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CatsRepository } from 'src/cats/cats.repository';
 import { LoginRequestDto } from './dto/login.request.dto';
 
@@ -8,5 +8,11 @@ export class AuthService {
 
   async jwtLogIn(data: LoginRequestDto) {
     const { email, password } = data;
+
+    const cat = await this.catsRepository.findOne({ email });
+
+    if (!cat) {
+      throw new UnauthorizedException('이메일과 비밀번호를 확인해 주세요.');
+    }
   }
 }
--- src/auth/dto/login.request.dto.ts
+++ src/auth/dto/login.request.dto.ts
@@ -1,5 +1,7 @@
-export class LoginRequestDto {
-  email: string;
+import { PickType } from '@nestjs/swagger';
+import { CatBaseDto } from 'src/dto/cat.base.dto';
 
-  password: string;
-}
+export class LoginRequestDto extends PickType(CatBaseDto, [
+  'email',
+  'password',
+] as const) {}
