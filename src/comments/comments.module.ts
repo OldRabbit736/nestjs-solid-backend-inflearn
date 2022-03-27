fdb8918831c0715ef73b7713f56a486bfdc3aaa1
OldRabbit736 <sylvan0212@gmail.com>
2022-03-27 16:06:26 +0900

    Add getAllComments service logic

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsModule } from 'src/cats/cats.module';
 import { CommentsController } from './controller/comments.controller';
+import { CommentsRepository } from './repository/comments.repository';
 import { CommentsService } from './service/comments.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommentsRepository]), CatsModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- src/comments/service/comments.service.ts
+++ src/comments/service/comments.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CatsRepository } from 'src/cats/repository/cats.repository';
 import { CreateCommentDto } from '../dto/create.comment.dto';
+import { CommentsRepository } from '../repository/comments.repository';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    private readonly commentsRepository: CommentsRepository,
+    private readonly catsRepository: CatsRepository,
+  ) {}
+
   getAllComments() {
-    return 'hello';
+    try {
+      return this.commentsRepository.find();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message); // HttpExceptionFilter에 걸릴 수 있도록
+    }
   }
 
   createComment(catid: string, comment: CreateCommentDto) {
