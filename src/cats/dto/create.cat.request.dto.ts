ad9aa2dbf047645df4f8bdff7b239b589fde6619
OldRabbit736 <sylvan0212@gmail.com>
2022-04-09 21:28:31 +0900

    DTO schema change

--- src/cats/dto/create.cat.request.dto.ts
+++ src/cats/dto/create.cat.request.dto.ts
@@ -1,8 +1,21 @@
 import { PickType } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { CatBaseDto } from './cat.base.dto';
 
 export class CreateCatRequestDto extends PickType(CatBaseDto, [
   'email',
   'password',
   'name',
-] as const) {}
+] as const) {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
